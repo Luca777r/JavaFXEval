c7cdc149401339a889c3e36fc6bf508725255bdf
Lucius <lucadelrosso.pro@gmail.com>
2023-02-13 13:21:51 +0100

    first commit

---
+++ src/main/java/com/evaluation/evalJavaFx/EvalJavaFxApplication.java
@@ -0,0 +1,13 @@
+package com.evaluation.evalJavaFx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalJavaFxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalJavaFxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evaluation/evalJavaFx/EvalJavaFxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluation.evalJavaFx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalJavaFxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
