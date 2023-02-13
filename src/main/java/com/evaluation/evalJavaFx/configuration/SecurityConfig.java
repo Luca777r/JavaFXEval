5fada9e1fcdebd07b75db46fd01b390710e2c9a6
Lucius <lucadelrosso.pro@gmail.com>
2023-02-13 13:52:55 +0100

    Security config

---
+++ src/main/java/com/evaluation/evalJavaFx/configuration/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.evaluation.evalJavaFx.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/user/**").permitAll()
+                .requestMatchers("/private/post/**").hasAnyRole("USER")
+                .anyRequest().authenticated()
+                .and().formLogin();
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            HttpSecurity http,
+            BCryptPasswordEncoder bCryptPasswordEncoder,
+            UserDetailsService userDetailService) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailService)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncore() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
